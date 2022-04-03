aead73bfdd8b59161cf29b3c2621ae4552ca0a6b
FlyAeroMan <93842887+FlyAeroMan@users.noreply.github.com>
2022-04-03 11:25:24 -0500

    Create PropulsionCalc.m
    PropulsionCalc.m - This will be the main entry point that calls the various functions.

---
+++ src/PropulsionCalc.m
@@ -0,0 +1,29 @@
+% PropulsionCalc - a set of MATLAB scripts to calculate turbojet properties
+% at various altitudes and compressor compression ratios.
+% Copyright (C) 2022  Kale Macormic
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+% April 03, 2022 11:20:59 AM CDT
+
+% NOTICE: This source code is Copyright (C) 2021  Kale Macormic and is
+% intended for AE502 Instructor/Grader use only. If you obtain this
+% document in error destroy it immediately.
+
+% initialize environment
+clear all
+close all
+clc
+
+% Call functions here...
